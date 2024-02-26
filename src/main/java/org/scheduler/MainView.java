e8deb021f771969dac347731835fc91c17ad0a53
Tomasz Siarkiewicz <tomasz.siarkiewicz@gmail.com>
2024-02-26 18:54:56 +0100

    Initial commit

---
+++ src/main/java/org/scheduler/MainView.java
@@ -0,0 +1,14 @@
+package org.scheduler;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("") // map view to the root
+class MainView extends VerticalLayout {
+    MainView() {
+        add(new H1("Hello, world"));
+        add(new H2("hello"));
+    }
+}
